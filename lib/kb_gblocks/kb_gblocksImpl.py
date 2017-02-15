9ce7628d1dbecda86af3f4300c1a3652bba21796
Dylan Chivian <dcchivian@lbl.gov>
2017-02-14 19:48:28 -0800

    debugging

--- lib/kb_gblocks/kb_gblocksImpl.py
+++ lib/kb_gblocks/kb_gblocksImpl.py
@@ -459,6 +459,7 @@ class kb_gblocks:
         # Gblocks names output blocks MSA by appending "-gb" to input file
         #output_GBLOCKS_file_path = os.path.join(output_dir, input_name+'-gb')
         output_GBLOCKS_file_path = input_MSA_file_path+'-gb'
+        output_aln_file_path = output_GBLOCKS_file_path
 
         # Gblocks is interactive and only accepts args from pipe input
         #if 'arg' in params and params['arg'] != None and params['arg'] != 0:
